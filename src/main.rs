9790417ba603c2325a07e5720810fad8c9ea2fc4
MFQWKMR4 <kenfan.fun@icloud.com>
2023-01-22 19:32:21 +0900

    test

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use anyhow::{Context, Result};
+use std::fs;
+use std::path::{Path, PathBuf};
 use std::process::{Command, Stdio};
 
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
@@ -6,7 +8,9 @@ fn main() -> Result<()> {
     let args: Vec<_> = std::env::args().collect();
     let command = &args[3];
     let command_args = &args[4..];
-    let output = Command::new(command)
+    let root = "./.sandbox";
+    let new_command = isolate(root, &command);
+    let output = Command::new(new_command)
         .args(command_args)
         .stdout(Stdio::inherit())
         .stderr(Stdio::inherit())
@@ -19,4 +23,33 @@ fn main() -> Result<()> {
         })?;
 
     std::process::exit(output.status.code().unwrap())
+}
+
+fn isolate(root: &str, command: &str) -> String {
+    let mut command_path = PathBuf::from(command);
+    let filename = path.file_name().unwrap().to_str().unwrap();
+    command_path.pop();
+
+    // mkdir virtual root
+    mkdir_p(PathBuf::from(root));
+
+    // prepare dev/null
+    mkdir_p(PathBuf::from(format!("{}/dev", root)));
+    fs::File::create(format!("{}/dev/null", root)).expect("creation of null file failed");
+
+    // copy command
+    mkdir_p(PathBuf::from(format!("{}/{}", root, command_path)));
+    let new_command = format!("{}/{}/{}", root, command_path, file_name);
+    fs::copy(command, new_command).expect("could not copy command");
+
+    // chroot virtual root
+    chroot(root).expect("not possible to use chroot");
+    new_command
+}
+
+fn mkdir_p(path: PathBuf) {
+    let display = path.display();
+    if !Path::new(&format!("{}", display)).exists() {
+        fs::create_dir(display).expect(&format!("not possible to create dir, {}", display));
+    }
 }
